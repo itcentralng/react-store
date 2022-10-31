a5c1de638b1f2aa2b80e84c3b44ef6bd336d49fb
khaleefa <mdcarliey@gmail.com>
2022-10-31 11:42:04 +0100

    changed footer and currency

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,42 +1,74 @@
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-import { useDispatch } from 'react-redux';
-import { addItem } from '../store/slices/cartSlice';
+import { useDispatch } from "react-redux";
+import { addItem } from "../store/slices/cartSlice";
 
-export default function ProductCard(props){
-    const { product }= props;
-    const dispatch = useDispatch();
+export default function ProductCard(props) {
+  const { product } = props;
+  const dispatch = useDispatch();
 
-    return (
-        <Card variant="outlined"  sx={{p:1,border:'1px solid #dbdbdb'}}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="240"
-                image={product.image}
-            />
-            <CardContent sx={{mt:1,borderTop:'1px solid #dbdbdb'}}>
-                <Typography gutterBottom variant="h6" component="div">
-                    { product.title }
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    { product.description.slice(0,100) }...
-                </Typography>
-            </CardContent>
-            <Box sx={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
-                <Typography sx={{ml:1}} variant="subtitle2" gutterBottom component="h6">
-                { product.price } $
-                </Typography>
-                <CardActions>
-                    <Button  variant="contained" size="small" onClick={() => dispatch(addItem(product))}>Add to Cart</Button>
-                </CardActions>
-            </Box>
-        </Card>
-    )
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        p: 1,
+        border: "1px solid #dbdbdb",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="240"
+        image={product.image}
+      />
+      <CardContent
+        sx={{
+          mt: 1,
+          borderTop: "1px solid #dbdbdb",
+          flex: 1,
+        }}
+      >
+        <Typography gutterBottom variant="h6" component="div">
+          {product.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{}}>
+          {product.description.slice(0, 100)}...
+        </Typography>
+      </CardContent>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          sx={{ ml: 1 }}
+          variant="subtitle2"
+          gutterBottom
+          component="h6"
+        >
+          ₦{product.price}
+        </Typography>
+        <CardActions>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => dispatch(addItem(product))}
+          >
+            Add to Cart
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
+  );
 }
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,41 +1,46 @@
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Layout from "../layouts/Layout";
 import ProductCard from "../../components/ProductCard";
 import { getItemsAsync } from "../../store/slices/productSlice";
 
-export default function Dashboard(){
-    const dispatch = useDispatch();
-    const productList=useSelector(state => state.product.productItems);
-    useEffect(()=>{
-        getProductItems();
-    },[]);
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.product.productItems);
+  useEffect(() => {
+    getProductItems();
+  }, []);
 
-    function getProductItems(){
-        dispatch(getItemsAsync());
-    }
-    return (
-        <Layout>
-            {
-                productList.length===0 ?
-                    <Box sx={{mt:4,display:'flex',justifyContent:'center',alignItems:'center'}}>
-                        <CircularProgress /> &nbsp;&nbsp;<strong>Loading...</strong>
-                    </Box>
-                 :
-                <Grid container spacing={2} >
-                    {productList.map((product) => {
-                        return (
-                            <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                                <ProductCard product={product} />
-                            </Grid>
-                        )
-                    })}
-                    
-                </Grid>
-            }
-        </Layout>
-    );
+  function getProductItems() {
+    dispatch(getItemsAsync());
+  }
+  return (
+    <Layout>
+      {productList.length === 0 ? (
+        <Box
+          sx={{
+            mt: 4,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress /> &nbsp;&nbsp;<strong>Loading...</strong>
+        </Box>
+      ) : (
+        <Grid container spacing={4}>
+          {productList.map((product) => {
+            return (
+              <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                <ProductCard product={product} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </Layout>
+  );
 }
